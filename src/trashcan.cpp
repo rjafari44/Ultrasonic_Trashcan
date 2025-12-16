5e81ee1731a6af579d66a95c3d3e89d5593cae3f
Radin <radin44@protonmail.com>
2025-12-16 11:52:47 -0800

    Completed trashcan code

--- src/trashcan.cpp
+++ src/trashcan.cpp
@@ -1,10 +1,6 @@
 #include "myheader.h"
 #include <Arduino.h>
 #include <ESP32Servo.h>
-#include <WiFi.h>
-
-const char* ssid = "ssid";
-const char* password = "password";
 
 static Servo controlServo{};
 const int servoPin{10};
@@ -12,27 +8,40 @@ const int servoPin{10};
 const int trigPin = 3;
 const int echoPin = 4;
 
+long duration{};
+long distance{};
+double soundSpeed{0.0343}; // speed of sound in centimeters per microseconds
+
 static char input{};
 
 void trashcan_setup() {
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
 
-    if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("Connected!");
-    Serial.println(WiFi.localIP());
-    }
-    else {
-    Serial.println("Not connected");
-    }
-
+    controlServo.attach(servoPin);
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
+    Serial.println("\n*** Ultrasonic Trashcan Mode ***");
+    Serial.println("Ultrasonic Trashcan is ready for use. Type 'm' or 'M' + Enter to return to mode menu");
 }
 
 void trashcan_loop() {
+    
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
 
-    // 
+    duration = pulseIn(echoPin, HIGH);      // returns total time it takes to go out and come back in microseconds
+    distance = (duration * soundSpeed) / 2; // multiply the total time by the speed of sound and divide by 2 to get just the distance to an object in centimeters
+
+    if (distance < 20) {
+        Serial.println("Trashcan is opening!\n");
+        controlServo.write(90); // open trashcan
+        delay(5000); // wait 5 seconds before closing
+        Serial.println("Trashcan is closing!\n");
+        controlServo.write(0); // close trashcan
+        delay(500); // wait for trashcan to close
+    }
 
     if (Serial.available() > 0) {        // If a character is waiting in the Serial input buffer, read it
         input = Serial.read();
